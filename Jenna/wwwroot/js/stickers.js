c988d80e380db30cfdbe20d4320f511c7c840b9c
JM <132931580+JXMakin@users.noreply.github.com>
2025-01-19 12:13:23 -0400

    Add files via upload

---
+++ Jenna/wwwroot/js/stickers.js
@@ -0,0 +1,23 @@
+const go_btn = document.getElementById("go-button");
+const sticker_pkg =document.getElementById("sticker-pkg");
+
+
+go_btn.addEventListener('click', open_pkg);
+
+function open_pkg() {
+    let random = Math.random()
+
+    if (random <= 0.1){
+
+    }
+    
+    else if (random <= 0.3){
+
+    }
+
+    else{
+
+    }
+    
+    window.open(indexedDB.html);
+}
